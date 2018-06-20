e5160db624dd672174aa5129e21b9699c49f82bd
davidtin <davidtin@opdaconsulting.com>
2018-06-20 14:31:01 -0400

    Update READme.md

--- src/App.js
+++ src/App.js
@@ -43,7 +43,16 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ol>
+            <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
+            <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
+            <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
+            <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+            <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
+            <li>When you click a CharComponent, it should be removed from the entered text.</li>
+          </ol>
+          <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+          <hr />
         </p>
         <input type="text" onChange={(event) => this.inputChangeHandler(event)} value={this.state.word}/>
        <ValidationComponent txtlength={this.state.txtlength} />
